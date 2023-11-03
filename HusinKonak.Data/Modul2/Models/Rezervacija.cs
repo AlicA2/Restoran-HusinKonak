b8d834e6f9948348ff8baa323a8b8a37837effe9
Dženan Magrdžija <dzenan.magrdzija@edu.fit.ba>
2023-11-03 23:35:29 +0100

    rezervacija edited

--- HusinKonak.Data/Modul2/Models/Rezervacija.cs
+++ HusinKonak.Data/Modul2/Models/Rezervacija.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,9 @@ namespace HusinKonak.Data.Modul2.Models
         public DateTime Vrijeme { get; set; }
         public string Prezime { get; set; }
         public string BrojOsoba { get; set; }
-        public bool Rezervisano { get; set; }
-
+        public string Rezervisano { get; set; }
+        [ForeignKey(nameof(korisnik))]
+        public int? korisnik_id { get; set; }
+        public Korisnik korisnik { get; set; }
     }
 }
---
+++ HusinKonak.Data/ViewModels/RezervacijaUpdateVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HusinKonak.Data.ViewModels
+{
+    public class RezervacijaUpdateVM
+    {
+        public string Rezervisano { get; set; }
+    }
+}
